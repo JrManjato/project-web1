992a0b11ef93f80463b171b3fd01a0882cef2a7f
Judicael RANDRIAMANJATO <judi.randriamanjato@gmail.com>
2022-03-11 22:55:41 +0300

    feat: Add JS files

--- js/main.js
+++ js/main.js
@@ -50,3 +50,23 @@ $(document).ready(function() {
 		nav.toggleClass('open');
 	});
 });
+
+	// Reveal an element
+const ratio = .1;
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+  const handleIntersect = function (entries, observer){
+    entries.forEach(function(entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+  }
+  const observer = new IntersectionObserver(handleIntersect, options);
+    document.querySelectorAll('[class*="reveal-"]').forEach(function(r){
+    observer.observe(r)
+  })
